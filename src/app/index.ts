594286bbe8979d157708375c4e843339c83f4988
Aniket Shukla <student.anishu@gmail.com>
2024-02-06 16:29:12 +0530

    Init

---
+++ src/app/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+
+import {User} from './user'
+
+export async function initServer() {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+            ${User.types}
+
+            type Query {
+                ${User.queries}
+            }
+        `,
+        resolvers: {
+            Query: {
+                ...User.resolvers.queries,
+            },
+        },
+    });
+    await graphqlServer.start();
+    app.use("/graphql", expressMiddleware(graphqlServer));
+
+    return app;
+}
---
+++ src/app/user/types.ts
@@ -0,0 +1,11 @@
+export const types = `#graphql
+          
+    type User {
+        id: ID!
+        firstName: String!
+        lastname: String!
+        email: String!
+        profileImageURL: String
+    }
+
+`;
